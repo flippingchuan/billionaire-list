ba4389c2d0a335af6bc4090c41ef2b5b7b7d725f
Chuan <shuochuan.xu@dowjones.com>
2016-02-23 16:54:01 -0500

    phase 1 configuration of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,50 @@
 var gulp = require('gulp'),
 		sass = require('gulp-sass'),
+		jshint = require('gulp-jshint'),
 		uglify = require('gulp-uglify'),
-		concat  = require('gulp-concat');
+		concat  = require('gulp-concat'),
+		sourcemaps = require('gulp-sourcemaps'),
+		clean = require('gulp-clean');
 
-gulp.task('sass', function () {
-  return gulp.src('./assets/sass/**/*.scss')
+var srcPaths = {
+  scripts: './src/js/**/*.js',
+  sass: './src/scss/**/*.scss'
+};
+var destPath = './dest';
+
+gulp.task('clean', function(){
+  return gulp.src(destPath + '/*', {read: false})
+		.pipe(clean());
+});
+
+gulp.task('sass', function (){
+  return gulp.src(srcPaths.sass)
+  	.pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dest'));
+    .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(destPath));
 });
 
-gulp.task('default', function(){
-	gulp.src('./assets/js/*.js')
-	.pipe(uglify())
-	.pipe(concat('main.js'))
-	.pipe(gulp.dest('./dest'));
+gulp.task('lint', function(){
+  return gulp.src(srcPaths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('scripts', function(){
+	gulp.src(srcPaths.scripts)
+	.pipe(sourcemaps.init())
+	.pipe(uglify())
+	.pipe(concat('main.min.js'))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest(destPath));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function(){
+  gulp.watch(srcPaths.scripts, ['lint', 'scripts']);
+  gulp.watch(srcPaths.sass, ['sass']);
+});
+
+gulp.task('default', ['watch', 'scripts', 'sass']);
